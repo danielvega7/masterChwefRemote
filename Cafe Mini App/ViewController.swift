7ffca4ec1a53ebc5e4f8db74b42b4f7b2bd1204e
Daniel Vega <daniel.vega23@d155.org>
2021-09-14 08:09:38 -0500

    add textfields

--- Cafe Mini App/ViewController.swift
+++ Cafe Mini App/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var foodTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+//    func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        
+//    }
     
     
 }
---
+++ Cafe Mini App/ViewController2.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController2.swift
+//  Cafe Mini App
+//
+//  Created by DANIEL VEGA on 9/14/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    
+var name = ""
+var price = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
