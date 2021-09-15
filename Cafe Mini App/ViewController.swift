d55dc58afc7475c4729ec0781a8d14ed05fdced2
Daniel Vega <daniel.vega23@d155.org>
2021-09-15 08:11:21 -0500

    added to both view controllers

--- Cafe Mini App/ViewController.swift
+++ Cafe Mini App/ViewController.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
+class SuperMegaVariables{
+    
+static var daFood = [String]()
+static var daPrice = [Double]()
+static var count = 0
+}
+
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var foodTextField: UITextField!
     @IBOutlet weak var priceTextField: UITextField!
     
-    
+    var hereCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        if (segue.identifier == "toAdmin") {
+        let nvc = segue.destination as! ViewController2
+        nvc.name = foodTextField.text!
+        nvc.price = Double(priceTextField.text!)!
+        }
+    }
     
+    @IBAction func adminAction(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "toAdmin", sender: nil)
+        
+    }
     
-//    func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        
-//    }
+    @IBAction func addToCart(_ sender: UIButton) {
+        SuperMegaVariables.daFood.append(foodTextField.text!)
+        SuperMegaVariables.daPrice.append(Double(priceTextField.text!)!)
+        hereCount += 1
+        SuperMegaVariables.count += 1
+        print(SuperMegaVariables.daFood, SuperMegaVariables.daPrice)
+    }
     
     
 }
--- Cafe Mini App/ViewController2.swift
+++ Cafe Mini App/ViewController2.swift
@@ -11,23 +11,19 @@ class ViewController2: UIViewController {
 
     
 var name = ""
-var price = 0
+    var price = 0.0
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(name, price)
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func transferToCart(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    
 }
